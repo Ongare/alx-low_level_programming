38eb80b94afec7f4306cce1f497999da828f283a
Ongare <stephenongare2966@gmail.com>
2023-03-11 13:52:04 -0800

    0x09

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,28 +1,46 @@
 #include "main.h"
+#include "2-strlen.c"
+
 /**
- * _atoi - convert a string into an integer.
+ * _atoi - converts string to integer
+ * @s: string to convert
  *
- * @s: the string to use.
- *
- * Return: integer.
+ * Return: returns integer value
  */
 int _atoi(char *s)
 {
- 	int sign = 1, i = 0;
- 	unsigned int res = 0;
+	int i;
+	int np = 0;
+	int c;
+	int d = 1;
+	int num = 0;
 
-
- 	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
- 	{
- 		if (s[i] == '-')
- 			sign *= -1;
- 		i++;
- 	}
- 	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
- 	{
- 		res = (res * 10) + (s[i] - '0');
- 		i++;
- 	}
- 	res *= sign;
- 	return (res);
- }
+	for (i = 0; i < _strlen(s); i++)
+	{
+		if (!(s[i] >= '0' && s[i] <= '9') && c > 0)
+			break;
+		if (s[i] == '-')
+			np--;
+		if (s[i] == '+')
+			np++;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			c++;
+		}
+	}
+	while (c > 0)
+	{
+		num += ((s[i - 1] - '0') * d);
+		i--;
+		c--;
+		d *= 10;
+	}
+	if (np >= 0)
+	{
+		num *= 1;
+	} else
+	{
+		num *= -1;
+	}
+	return (num);
+}
