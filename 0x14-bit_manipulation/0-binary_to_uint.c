bdbec69ffed3aed23a431a0704e8522655afcbfa
Ongare <stephenongare2966@gmail.com>
2023-04-09 05:40:57 -0700

    correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,31 +7,27 @@
  *
  * Return: unsigned int.
  */
-unsigned int binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char *binary)
 {
-	unsigned int ui;
-	int len, base_two;
+    unsigned int decimal = 0;  
+    int len = strlen(binary);  
 
-	if (!b)
-		return (0);
+    for (int i = 0; i < len; i++) {
+        if (binary[i] == '1') {
 
-	ui = 0;
+            decimal = (decimal << 1) | 1;
+        }
 
-	for (len = 0; b[len] != '\0'; len++)
-		;
+        else if (binary[i] == '0') {
 
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
-	{
-		if (b[len] != '0' && b[len] != '1')
-		{
-			return (0);
-		}
+            decimal = decimal << 1;
+        }
+        else {
+            printf("Error: Invalid binary character '%c'\n", binary[i]);
+            exit(1);
+        }
+    }
 
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
-	}
-
-	return (ui);
+    return decimal;
 }
+
