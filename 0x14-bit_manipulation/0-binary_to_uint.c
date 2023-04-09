ba808b2629a695aca716dff52920cef88bc8c023
Ongare <stephenongare2966@gmail.com>
2023-04-09 06:00:34 -0700

    cor

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,26 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+unsigned int uint_num = 0;
+	int bit_pos = 0;
 
-	if (!b)
-		return (0);
-
-	for (i = 0; b[i]; i++)
+	if (b == NULL)
 	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		return (0);
 	}
-	return (dec_val);
+	while (b[bit_pos] != '\0')
+	{
+		if (b[bit_pos] != '0' && b[bit_pos] != '1')
+		{
+			return (0);
+		}
+		uint_num <<= 1;
+		if (b[bit_pos] == '1')
+		{
+			uint_num |= 1;
+		}
+		bit_pos++;
+	}
+
+	return (uint_num);
 }
